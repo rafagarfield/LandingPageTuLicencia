4488adc1ed7de70bbc0b2a2fcf281f6e3b61fe4b
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-01-15 11:56:56 -0500

    add About component to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from '@/components/NavBar'
 import Hero from '@/components/Hero'
+import About from '@/components/About'
 function page() {
   return (
     <div>
       <Navbar />
+      <div className=' relative top-[70px] md:top-[70px] lg:top-[75px]'>
       <Hero />
+      <About />
+      </div>
+      
     </div>
   )
 }
