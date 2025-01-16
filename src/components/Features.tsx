e3a184162a1b733a8510890dd85b722ef1097143
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-01-16 10:48:59 -0500

    add car SVG icon and update Features component to use SVG

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -7,7 +7,7 @@ const Features = () => {
     
       <div className="relative flex justify-center items-center w-full lg:h-[1000px] h-[520px] bg-[url('/icon/purple.svg')] bg-cover bg-custom-position-mobile md:bg-custom-position-lg lg:bg-custom-position-lg  bg-no-repeat waveContainer waveContainerMobile ">
         <Image
-          src="/images/car.png"
+          src="/icon/car.svg"
           alt="Purple Background"
           width={150}
           height={150}
