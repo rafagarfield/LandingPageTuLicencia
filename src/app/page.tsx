2a7085f6a05c5ef5e05081a987afd36fd574f6c0
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-01-16 10:25:08 -0500

    add Features component to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '@/components/NavBar'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
+import Features from '@/components/Features'
+
 function page() {
   return (
     <div>
@@ -9,8 +11,8 @@ function page() {
       <div className=' relative top-[70px] md:top-[70px] lg:top-[75px]'>
       <Hero />
       <About />
+      <Features />
       </div>
-      
     </div>
   )
 }
