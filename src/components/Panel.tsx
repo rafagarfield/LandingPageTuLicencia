da741dab8994a935b8abbeee9ecb35cdc124f8b2
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2025-01-17 09:33:50 -0500

    refactor Panel and Procedure components for improved layout and responsiveness

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -3,25 +3,29 @@ import React from "react";
 
 function Panel() {
   return (
-    <div className="bg-[#11793D] mx-[24px] p-4 rounded-[36px] flex flex-col justify-center items-center gap-6 mb-10">
-      <h2 className="text-[24px] font-bold text-[#ffffff] text-center">
-        Una nueva manera de realizar tus trámites
-      </h2>
-      <hr className="h-[2px] bg-white border-0 w-[50%] text-center" />
-      <p className=" text-[#fff] text-center text-[16px] ">
-        Utiliza las ventajas de la comunicación y procesos digitales con el
-        apoyo inmediato y constante de nuestros especialistas, disponibles por
-        teléfono, chat y WhatsApp a tu conveniencia
-      </p>
-      <button className=" bg-white border border-[#fff] rounded-[25px] px-[32px] py-[14px] text-[#11793d] text-[16px] font-bold">
-        Comienza ahora
-      </button>
-      <div>
+    <div className="bg-[#11793D] mx-[24px] px-4 py-8 rounded-[36px] flex flex-col justify-center items-center gap-6 mb-10 lg:grid lg:grid-cols-2 lg:p-20 ">
+      <div className="flex flex-col justify-center items-center gap-8 lg:items-start">
+        <h2 className="text-[24px] font-bold text-[#ffffff] text-center">
+          Una nueva manera de realizar tus trámites
+        </h2>
+        <hr className="h-[2px] bg-white border-0 w-[40%] text-center" />
+        <p className=" text-[#fff] text-center text-[16px] lg:text-start ">
+          Utiliza las ventajas de la comunicación y procesos digitales con el
+          apoyo inmediato y constante de nuestros especialistas, disponibles por
+          teléfono, chat y WhatsApp a tu conveniencia
+        </p>
+        <button className=" bg-white border border-[#fff] rounded-[25px] px-[32px] py-[14px] text-[#11793d] text-[16px] font-bold">
+          Comienza ahora
+        </button>
+      </div>
+
+      <div className="flex justify-center items-center">
         <Image
           src="/icon/panel.svg"
           alt="Trámites de personas"
           width={200}
           height={200}
+          className="lg:w-[300px] lg:h-[300px]"
         />
       </div>
     </div>
--- src/components/Procedure.tsx
+++ src/components/Procedure.tsx
@@ -1,17 +1,20 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 function Procedure() {
   return (
-    <div className='mb-10'>
-      <div className='flex flex-col justify-center items-center gap-4 '>
-        <h2 className='text-[#11793D] text-[24px] font-bold '  >¿Estás Listo?</h2>
-        <p className='text-[#717171] text-[20px] text-center'>Selecciona el servicio que necesitas</p>
+   
+    <div className="mb-10">
+      <div className="flex flex-col justify-center items-center gap-4 ">
+        <h2 className="text-[#11793D] text-[24px] font-bold ">¿Estás Listo?</h2>
+        <p className="text-[#717171] text-[20px] text-center">
+          Selecciona el servicio que necesitas
+        </p>
       </div>
-      <div className='flex flex-col gap-2 justify-center items-center p-2'>
-        {/* Trámites de personas */}
-        <div className='flex flex-col justify-center items-center'>
-          <div >
+
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-row  gap-4 lg:gap-[150px]  justify-center items-center">
+          <div className="lg:w-[300px] flex justify-center items-center">
             <Image
               src="/icon/personProcedure.svg"
               alt="Trámites de personas"
@@ -19,19 +22,23 @@ function Procedure() {
               height={200}
             />
           </div>
-          <button className='px-[20px] py-[10px] lg:px-[32px] lg:py-[14px] border-[1px] border-[#1A7261] rounded-[25px] text-[#1A7261] text-center text-[16px] font-medium whitespace-nowrap '>Trámites de personas</button>
-        </div>
-        {/* Trámites de vehículos */}
-        <div className='flex flex-col justify-center items-center'>
-          <div >
+          <div className=" lg:w-[300px] flex justify-center items-center">
             <Image
               src="/icon/carProcedure.svg"
-              alt="Trámites de vehículos"
+              alt="Trámites de Vehiculos"
               width={200}
               height={200}
             />
           </div>
-          <button className='px-[20px] py-[10px] lg:px-[32px] lg:py-[14px] border-[1px] border-[#1A7261] rounded-[25px] text-[#1A7261] text-center text-[16px] font-medium whitespace-nowrap min-w-[150px]'>Trámites de vehículos</button>
+        </div>
+
+        <div className="flex flex-col gap-4 lg:gap-[150px] lg:flex-row  p-4   ">
+          <button className="px-[20px] py-[10px]   border-[1px] border-[#1A7261] rounded-[25px] text-[#1A7261] text-center text-[16px] font-medium w-[240px] lg:w-[300px]">
+            Trámites de personas
+          </button>
+          <button className="px-[20px] py-[10px]   border-[1px] border-[#1A7261] rounded-[25px] text-[#1A7261] text-center text-[16px] font-medium w-[240px] lg:w-[300px]  ">
+            Trámites de vehículos
+          </button>
         </div>
       </div>
     </div>
